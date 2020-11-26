a3176bb349f2be6ec6463fa96d540a61e0171236
nicolnasr <38070735+nicolnasr@users.noreply.github.com>
2020-11-26 12:56:22 +0200

    Create Hidden.py

---
+++ Hidden.py
@@ -0,0 +1,5 @@
+def oauth():
+    return {"consumer_key": "q5...v1",
+            "consumer_secret": "RY0...Ll963",
+            "token_key": "44589...hk",
+            "token_secret": "LK...o6HsQl"}
